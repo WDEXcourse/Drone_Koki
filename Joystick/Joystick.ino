d5a1836a96481f3009e2d2033fdb11c7b15fcb45
kouki <kokikokikoki033@gmail.com>
2023-03-19 18:01:01 +0900

    コードを追加した。

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -14,6 +14,8 @@ const int Ymin_LED = 6;
 const int Ymax_LED = 9;
 
 
+
+
 int X_POS ;
 
 int Y_POS ;
@@ -36,13 +38,13 @@ void setup() {
   pinMode(SW_PIN,INPUT_PULLUP);
 
 
-  pinMode(X_min_LED,OUTPUT);
+  pinMode(Xmin_LED,OUTPUT);
 
-  pinMode(X_mix_LED,OUTPUT);
+  pinMode(Xmin_LED,OUTPUT);
 
   pinMode(Ymin_LED,OUTPUT);
 
-  pinMode(Ymix,OUTPUT);
+  pinMode(Ymin_LED,OUTPUT);
 
 
   pinMode(SW_PIN,OUTPUT);
@@ -61,7 +63,36 @@ void loop() {
   SW_POS = digitalRead(SW_PIN);
 
 
-  if((SW_P
+  if(SW_POS == 0){
+   
+    digitalWrite(SW_PIN,HIGH);
+  }else{
+    digitalWrite(SW_PIN,LOW);
+  }
+
+  if((X_POS > 512-512*OFFSET) && (X_POS < 512+512*OFFSET)){
+    analogWrite(Xmin_LED,0);
+    analogWrite(Xmax_LED,0);
+  }
+
+  if((Y_POS > 512-512*OFFSET) && (Y_POS < 512-512*OFFSET)){
+    analogWrite(Ymin_LED,0);
+    analogWrite(Ymax_LED,0);
+  }
+
+
+  if (X_POS <= 512-512*OFFSET)
+  {
+   DUTY = map(X_POS) /* code */
+  }
+  
+
+
+  
+  
+    
+  
+  
 
   
 
